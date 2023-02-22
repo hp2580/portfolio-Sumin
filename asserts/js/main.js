fe39017dd6f1c971628736ea7f6d7ed4bcaaf483
sumin <happycandy0909@gamil.com>
2023-02-22 15:45:31 +0900

    Setting hScroll(Project Sectoin)

--- asserts/js/main.js
+++ asserts/js/main.js
@@ -1,5 +1,17 @@
-const scrollBar = document.querySelector(".scrollbar .bar");
-const portFolio = document.querySelector(".contentWrap h1");
+const scrollBar = document.querySelector(".scrollbar .bar"),
+  portFolio = document.querySelector(".contentWrap h1"),
+  project = document.querySelector(".projects"),
+  sticky = document.querySelector(".stickyWrap");
+
+const hScroll = () => {
+  let top = project.getBoundingClientRect().top;
+  let stickyTop = sticky.getBoundingClientRect().top;
+  let scrollWidth = sticky.scrollWidth;
+  let scrollHeight = project.scrollHeight - sticky.scrollHeight;
+  if (stickyTop < 1) {
+    sticky.scrollLeft = (scrollWidth / scrollHeight) * -top * 0.8;
+  }
+};
 
 window.addEventListener("DOMContentLoaded", () => {
   let splitText = portFolio.innerText.split("");
@@ -8,9 +20,6 @@ window.addEventListener("DOMContentLoaded", () => {
     let char = document.createElement("span");
     char.innerText = text;
     char.style.display = `inline-block`;
-    // char.style.animation = `moveTextUpDown .9s ${
-    //   idx * 0.1
-    // }s infinite alternate-reverse`;
     char.style.animationDelay = `${idx * 0.1}s`;
     portFolio.append(char);
   });
@@ -20,4 +29,6 @@ window.onscroll = () => {
   let y = window.scrollY;
   let percent = (y / (document.body.scrollHeight - window.innerHeight)) * 100;
   scrollBar.style.width = `${percent}%`;
+
+  hScroll();
 };
